87b2727e010fe4986232c806e1a860c80fe44ba8
Amrani005 <superamrani@gmail.com>
2025-11-16 23:55:17 +0100

    fixng negga

---
+++ app/productinfo/loading.tsx
@@ -0,0 +1,11 @@
+// This file should be at app/productinfo/loading.tsx
+
+export default function Loading() {
+  // You can make this look as good as you want.
+  // For now, simple text is fine.
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <p className="text-white text-2xl">Loading product...</p>
+    </div>
+  );
+}
